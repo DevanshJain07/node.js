bd61bb85d4ed77c6c65cfa3a8e4e6de4ea124baa
devansh071999 <devanshsampatjain@gmail.com>
2020-01-01 20:00:04 +0530

    read,write,create,destroy

---
+++ credesdir.js
@@ -0,0 +1,12 @@
+var fs=require('fs');
+ //fs.mkdirSync('stuff');
+//fs.rmdirSync('stuff');
+fs.mkdir('stuff',function(){
+ fs.readFile('readMe.txt','utf8',function(err,data){
+  fs.writeFile('./stuff/writeMe.txt',data);
+ });
+});
+
+
+
+
---
+++ readwrite.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var readMe=fs.readFileSync('readMe.txt','utf8');
+console.log(readMe);
+fs.writeFileSync('writeMe.txt',readMe);
+fs.readFile('readMe.txt','utf8',function(err,data){
+    console.log(data);
+});
+console.log('test');
+
+fs.unlink('writeMe.txt');
+
+
+
+
