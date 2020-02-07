296babb26493925b913afa99e5083540dde4e7be
devansh071999 <devanshsampatjain@gmail.com>
2020-02-08 01:15:17 +0530

    ejs files added

--- usingexpress.js
+++ usingexpress.js
@@ -2,10 +2,12 @@ var express = require('express')
 var app = express();
 app.set('view engine','ejs');
 app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html');
+//res.sendFile(__dirname + '/index.html');
+   res.render('index');
 });
-app.get('/contact',function(res,req){
-    res.sendFile(__dirname +'contactus.html');
+app.get('/contact',function(req,res){
+   //res.sendFile(__dirname +'contactus.html');
+   res.render('contact');
 });
 app.get('/profile/:name',function(req,res){
 // res.send('You requested to see a profile with the name of '+ req.params.name);
